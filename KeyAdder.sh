1dc4fa029ffa122737d631c200a25d9d83d58b84
Shrh <52282777+Shr776@users.noreply.github.com>
2022-07-11 12:07:24 +0430

    Update KeyAdder.sh

--- KeyAdder.sh
+++ KeyAdder.sh
@@ -1,26 +1,67 @@
 #!/bin/sh
-# wget -q "--no-check-certificate" https://github.com/Shr776/PomLinux/raw/Plugins/KeyAdder.sh -O - | /bin/sh
-sleep 1
-echo "Old Version Deleting..."
-rm -rf /usr/lib/enigma2/python/Plugins/Extensions/KeyAdder
-rm -rf /tmp/KeyAdder.tar.gz
-sleep 1
-wget https://github.com/Shr776/PomLinux/raw/Plugins/KeyAdder.tar.gz -P /tmp
-echo "New Version Downloaded"
-sleep 1
-if [ -f /tmp/KeyAdder.tar.gz ]; then
-    mkdir /usr/lib/enigma2/python/Plugins/Extensions/KeyAdder
-	tar -xzf /tmp/KeyAdder.tar.gz -C /usr/lib/enigma2/python/Plugins/Extensions/KeyAdder
-fi
-sleep 2
-if [ -d /usr/lib/enigma2/python/Plugins/Extensions/KeyAdder ]; then
-	echo "New Version Installed"
-	rm -rf /tmp/KeyAdder.tar.gz
-	sleep 2
-	init 4
-	echo "Restarting Your Enigma2 Gui..."
-	init 3
+#########################################################
+version=5.9
+description="KeyAdder for Biss Key" !!!
+
+#########################################################
+
+#########################################################
+PACKAGE_DIR='KeyAdder/main'
+MY_FILE="KeyAdder.tar.gz"
+#########################################################
+
+MY_MAIN_URL="https://raw.githubusercontent.com/Shr776/"
+MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE
+MY_TMP_FILE="/tmp/"$MY_FILE
+
+rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+MY_SEP='============================================================='
+echo $MY_SEP
+echo 'Downloading '$MY_FILE' ...'
+echo $MY_SEP
+echo ''
+wget -T 2 $MY_URL -P "/tmp/"
+
+if [ -f $MY_TMP_FILE ]; then
+
+	echo ''
+	echo $MY_SEP
+	echo 'Extracting ...'
+	echo $MY_SEP
+	echo ''
+	tar -xf $MY_TMP_FILE -C /
+	MY_RESULT=$?
+
+	rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+	echo ''
+	echo ''
+	if [ $MY_RESULT -eq 0 ]; then
+        echo "#########################################################"
+        echo "#  KeyAdder Plugins $version INSTALLED SUCCESSFULLY      #"
+        echo "#                BY Shr776 - support on                   #"
+        echo "#   https://t.me/VuSolo      #"
+        echo "#########################################################"
+        echo "#           your Device will RESTART Now                #"
+        echo "#########################################################"		
+		if which systemctl > /dev/null 2>&1; then
+			sleep 2; systemctl restart enigma2
+		else
+			init 4; sleep 4; init 3;
+		fi
+	else
+		echo "   >>>>   INSTALLATION FAILED !   <<<<"
+	fi;
+	echo ''
+	echo '**************************************************'
+	echo '**                   FINISHED                   **'
+	echo '**************************************************'
+	echo ''
+	exit 0
 else
-	echo "New Version Failed To Load"
+	echo ''
+	echo "Download failed !"
+	exit 1
 fi
-exit 0
+# ------------------------------------------------------------------------------------------------------------
